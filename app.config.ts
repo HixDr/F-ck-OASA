848008a662d199137ddde47e2ca485800268366a
HixDr <eftihisdrakakis@gmail.com>
2026-02-22 17:46:00 +0200

    move google maps api key to env variable

---
+++ app.config.ts
@@ -0,0 +1,70 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "F*ck OASA",
+  slug: "fck-oasa",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "dark",
+  newArchEnabled: true,
+  scheme: "fck-oasa",
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#000000",
+  },
+  ios: {
+    supportsTablet: false,
+    bundleIdentifier: "com.itshix.fckoasa",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#000000",
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+    permissions: [
+      "ACCESS_FINE_LOCATION",
+      "ACCESS_COARSE_LOCATION",
+      "android.permission.ACCESS_COARSE_LOCATION",
+      "android.permission.ACCESS_FINE_LOCATION",
+      "android.permission.POST_NOTIFICATIONS",
+    ],
+    package: "com.itshix.fckoasa",
+    config: {
+      googleMaps: {
+        apiKey: process.env.GOOGLE_MAPS_API_KEY ?? "",
+      },
+    },
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-location",
+      {
+        locationWhenInUsePermission: "Used to find nearby bus stops.",
+      },
+    ],
+    "expo-font",
+    [
+      "expo-notifications",
+      {
+        icon: "./assets/notif-icon.png",
+        color: "#F59E0B",
+      },
+    ],
+    "./plugins/withAndroidOptimizations",
+  ],
+  extra: {
+    router: {},
+    eas: {
+      projectId: "4fab4ff0-2a2e-4acb-ba11-d827c6c0ad26",
+    },
+  },
+});
